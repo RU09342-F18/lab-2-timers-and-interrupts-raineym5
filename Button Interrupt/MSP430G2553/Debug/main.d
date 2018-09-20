d13bd323e42e3026c4fe2bd885175c909942dd16
raineym5 <raineym5@students.rowan.edu>
2018-09-20 08:57:44 -0400

    Button interrupt

---
+++ Button Interrupt/MSP430G2553/Debug/main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/matt/ti/ccsv8/ccs_base/msp430/include/msp430.h
+main.obj: /home/matt/ti/ccsv8/ccs_base/msp430/include/msp430g2553.h
+main.obj: /home/matt/ti/ccsv8/ccs_base/msp430/include/in430.h
+main.obj: /home/matt/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h
+main.obj: /home/matt/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h
+
+../main.c: 
+/home/matt/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+/home/matt/ti/ccsv8/ccs_base/msp430/include/msp430g2553.h: 
+/home/matt/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+/home/matt/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics.h: 
+/home/matt/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include/intrinsics_legacy_undefs.h: 
---
+++ Button Interrupt/MSP430G2553/main.c
@@ -0,0 +1,44 @@
+#include <msp430.h> 
+
+
+/**
+ * main.c
+ */
+int delayval = 0;counter=0;
+
+void main(void)
+{
+    P1DIR = 0xf7;//11110111
+    P2DIR = 0xff;
+    P1OUT = 0x00;
+    P2OUT = 0x00;
+    P1OUT = 0x00;
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    P1IE |= BIT3; // P1.3 interrupt enabled
+    P1IFG &= ~BIT3; // P1.3 IFG cleared
+    while(1){
+        __enable_interrupt(); // enable all interrupts
+        if(counter == 0)
+            P1OUT ^= BIT0;
+        counter++;
+        __delay_cycles(10000);
+        if(counter >= delayval)
+            counter=0;
+    }
+	
+}
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void){
+        P1IFG &= ~BIT3;
+        delayval=0;
+        counter=0;
+        P1OUT |= BIT3;
+
+        while(!(P1IN & BIT3)){
+            delayval++;
+            __delay_cycles(10000);
+        }
+        P1OUT &= ~(1<<BIT0);
+
+
+}

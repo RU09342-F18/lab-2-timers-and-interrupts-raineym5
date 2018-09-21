afdf1efb8fe2df050224c6cc95dfd2527e1d7192
raineym5 <raineym5@students.rowan.edu>
2018-09-20 21:19:10 -0400

    Need help finding proper documentation

--- TIMER A Blink/MSP430G2553/main.c
+++ TIMER A Blink/MSP430G2553/main.c
@@ -6,7 +6,28 @@
  */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+    P1DIR = 0xff;
+    P2DIR = 0xff;
+    P1OUT = 0x00;
+    P2OUT = 0x00;
+    P1OUT = 0x00;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    //TACTL = (1<<TASSEL0)|(0x11<<ID0)|(1<<MC0)|()
+    //Help, TI documentation sucks. I just need the register names, but names, and what they do
+    //TA0R
+    //TA0CTL
+    //TA0IV
+    while(1){
+        __enable_interrupt();
+    }
+
+}
+
+#pragma vector=TIMER1_A0_VECTOR
+__interrupt void TimerA0(void){
+        TA1CCTL0 &= ~CCIFG;
+        TA1CTL &= ~TAIFG;
+
+
+
 }

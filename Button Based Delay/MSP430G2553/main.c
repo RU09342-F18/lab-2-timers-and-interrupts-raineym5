05bd1940a5c5bc3b4a6c5044397b83ca93937a73
raineym5 <raineym5@students.rowan.edu>
2018-09-29 12:44:50 -0400

    Fixed ISR bug

--- Button Based Delay/MSP430G2553/main.c
+++ Button Based Delay/MSP430G2553/main.c
@@ -3,32 +3,39 @@
 
 /**
  * main.c
- * MOST OF THIS IS PSEUDO-CODE I WILL UPDATE THIS TOMORROW
  */
-unsigned volatile int delayval = 7;counter=0;//10 Hz
+unsigned volatile int delayval = 7, counter=0, butpress=0;//10 Hz
 
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void){
         P1IFG &= ~BIT3;
-        //if bit selection is low to high
-        P1OUT |= BIT0;
-        delayval=0;
-        counter=0;
-        //reset timer
+        if(P1IES && BIT3){
 
-        //else
-        P1OUT &= ~(1<<BIT0);
+            P1IES |= BIT3;
+            P1OUT |= BIT0;
+            TAR = 0;
+            delayval=0;
+            butpress=1;
+            counter=0;
+        }else{
+            P1OUT &= ~(1<<BIT0);
+            P1IES &= ~BIT3;
+            butpress=0;
+        }
 
-        //invert bit selection for but0
 
 
 }
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void timer_A0(void){
     //disable interrupts
-    delayval++
-    if(delayval == 100)
+    counter++;
+    if(butpress)
+        delayval++;
+    else if(counter >= delayval){
         P1OUT ^= 0x01;
+        counter = 0;
+    }
 
 }
 void main(void)
@@ -39,8 +46,11 @@ void main(void)
     P2OUT = 0x00;
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
     P1IE |= BIT3; // P1.3 interrupt enabled
-    P1IES |= BIT3;
+    P1IES |= BIT3;//bit selection
     P1IFG &= ~BIT3; // P1.3 IFG cleared
+    TA0CTL = (TASSEL1)|(MC0);    //set timer A control bits - source select and mode control
+    CCTL0 = CCIE;               // CCR0 interrupt enabled
+    CCR0 = 65535;               //set value for CCR0 interrupt to fire
     while(1){
         __enable_interrupt();
     }

a00a4e37aa73a8cf5486a23f25790da89f142d65
raineym5 <raineym5@students.rowan.edu>
2018-09-29 13:39:05 -0400

    fixed button interrupt

--- Button Based Delay/MSP430G2553/main.c
+++ Button Based Delay/MSP430G2553/main.c
@@ -9,28 +9,27 @@ unsigned volatile int delayval = 7, counter=0, butpress=0;//10 Hz
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void){
         P1IFG &= ~BIT3;
-        if(P1IES && BIT3){
+        if(P1IES & BIT3){
 
-            P1IES |= BIT3;
-            P1OUT |= BIT0;
-            TAR = 0;
-            delayval=0;
-            butpress=1;
-            counter=0;
+            P1IES &= ~BIT3;//enable logic high-low interrupt
+
+            P1OUT |= BIT0;//turn on LED
+            TA0R = 0;//set timer A0 to 0 (not needed but it gives cleaner operation)
+            delayval=0;//set delayval to 0
+            butpress=1;//set state machine bit
         }else{
-            P1OUT &= ~(1<<BIT0);
-            P1IES &= ~BIT3;
+            P1IES |= BIT3;//set low-high button interrupt
+            P1OUT &= ~BIT0;//disable output
             butpress=0;
+            counter=0;
         }
-
-
 
 }
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void timer_A0(void){
     //disable interrupts
     counter++;
-    if(butpress)
+    if(butpress == 1)//state machine bit. if one, number of timer overflows per led blink increases
         delayval++;
     else if(counter >= delayval){
         P1OUT ^= 0x01;
@@ -41,7 +40,7 @@ __interrupt void timer_A0(void){
 void main(void)
 {
     P1DIR = 0xf7;//11110111
-    P2DIR = 0xff;
+    P2DIR = 0xff;//initialize outputs to 0
     P1OUT = 0x00;
     P2OUT = 0x00;
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
@@ -50,7 +49,7 @@ void main(void)
     P1IFG &= ~BIT3; // P1.3 IFG cleared
     TA0CTL = (TASSEL1)|(MC0);    //set timer A control bits - source select and mode control
     CCTL0 = CCIE;               // CCR0 interrupt enabled
-    CCR0 = 65535;               //set value for CCR0 interrupt to fire
+    CCR0 = 8192;               //set value for CCR0 interrupt to fire
     while(1){
         __enable_interrupt();
     }
